3df0215dc01fcbf2e61ee876f03a606dab290681
iqpizza6349 <iqpizza6349@gmail.com>
2022-09-14 13:29:53 +0900

    :wastebasket: :: deprecated Injection.java(annotation)

--- src/main/java/com/eleven/casinobot/event/annotations/Injection.java
+++ src/main/java/com/eleven/casinobot/event/annotations/Injection.java
@@ -2,11 +2,9 @@ package com.eleven.casinobot.event.annotations;
 
 import java.lang.annotation.*;
 
+@Deprecated
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-@Inherited
 public @interface Injection {
-
     String name() default "";
-
 }
