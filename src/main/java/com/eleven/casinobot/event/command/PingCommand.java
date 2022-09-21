6c83687bbb230b5c9ef8b2a4738565be4272669f
iqpizza6349 <iqpizza6349@gmail.com>
2022-09-21 15:29:25 +0900

    :zap: :: improve ping command

--- src/main/java/com/eleven/casinobot/event/command/PingCommand.java
+++ src/main/java/com/eleven/casinobot/event/command/PingCommand.java
@@ -3,17 +3,18 @@ package com.eleven.casinobot.event.command;
 import com.eleven.casinobot.core.annotations.Command;
 import com.eleven.casinobot.core.command.CommandContext;
 import com.eleven.casinobot.core.command.ICommand;
-import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 @Command(value = "ping", description = "Calculate ping of the Bot")
 public class PingCommand implements ICommand {
 
     @Override
     public void onEvent(CommandContext ctx) {
-        MessageChannel channel = ctx.getEvent().getMessageChannel();
+        SlashCommandInteractionEvent event = ctx.getEvent();
         long time = System.currentTimeMillis();
-        channel.sendMessage("Pong!")
-                .queue(response -> response.editMessageFormat("Pong: %d ms",
-                        System.currentTimeMillis() - time).queue());
+        event.reply("Pong!").setEphemeral(true)
+                .flatMap(hook ->
+                        hook.editOriginalFormat("Pong: %d ms", System.currentTimeMillis() - time))
+                        .queue();
     }
 }
